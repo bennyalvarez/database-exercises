cc04d3f4346ef8fb4545ce3e4035c3b64e018a6f
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-06-17 16:33:40 -0500

    updated exercise after walkthrough

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,14 @@
 USE codeup_test_db;
 
-CREATE TABLE IF NOT EXISTS albums (
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
-    name VARCHAR(50),
-    release_date INT UNSIGNED NOT NULL,
-    sales DOUBLE UNSIGNED NOT NULL,
-    genre CHAR(50),
+    artist VARCHAR(50),
+    name VARCHAR(100),
+    release_date INT,
+    sales DOUBLE UNSIGNED NOT NULL,         --DECIMAL(15, 2), also would have been more precise since 10 digits in million.
+    genre VARCHAR(50),
     PRIMARY KEY (id)
 
 
