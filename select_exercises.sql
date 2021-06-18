ebf3939bbbb98535d16310eab87263e4d7e11bb8
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-06-18 14:18:59 -0500

    working on albums exercises

---
+++ select_exercises.sql
@@ -0,0 +1,5 @@
+USE codeup_test_db;
+
+SELECT name AS 'Info';
+
+SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club Band release date' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
